781cb60374d9e53a7324f76c1892f1e7582cdfb3
SANDBOX-09 <SANDBOX-09@SANDBOX-09>
2024-10-10 09:42:10 +0800

    Add project files.

---
+++ Employee1/Form1.Designer.vb
@@ -0,0 +1,91 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnadd = New System.Windows.Forms.Button()
+        Me.btnload = New System.Windows.Forms.Button()
+        Me.btnclose = New System.Windows.Forms.Button()
+        Me.datarecord = New System.Windows.Forms.DataGridView()
+        CType(Me.datarecord, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnadd
+        '
+        Me.btnadd.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnadd.Location = New System.Drawing.Point(71, 95)
+        Me.btnadd.Name = "btnadd"
+        Me.btnadd.Size = New System.Drawing.Size(75, 23)
+        Me.btnadd.TabIndex = 0
+        Me.btnadd.Text = "Add User"
+        Me.btnadd.UseVisualStyleBackColor = True
+        '
+        'btnload
+        '
+        Me.btnload.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnload.Location = New System.Drawing.Point(71, 142)
+        Me.btnload.Name = "btnload"
+        Me.btnload.Size = New System.Drawing.Size(75, 52)
+        Me.btnload.TabIndex = 1
+        Me.btnload.Text = "Load Record"
+        Me.btnload.UseVisualStyleBackColor = True
+        '
+        'btnclose
+        '
+        Me.btnclose.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnclose.Location = New System.Drawing.Point(71, 223)
+        Me.btnclose.Name = "btnclose"
+        Me.btnclose.Size = New System.Drawing.Size(75, 23)
+        Me.btnclose.TabIndex = 2
+        Me.btnclose.Text = "Close"
+        Me.btnclose.UseVisualStyleBackColor = True
+        '
+        'datarecord
+        '
+        Me.datarecord.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.datarecord.Location = New System.Drawing.Point(297, 54)
+        Me.datarecord.Name = "datarecord"
+        Me.datarecord.Size = New System.Drawing.Size(373, 238)
+        Me.datarecord.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.datarecord)
+        Me.Controls.Add(Me.btnclose)
+        Me.Controls.Add(Me.btnload)
+        Me.Controls.Add(Me.btnadd)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.datarecord, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnadd As Button
+    Friend WithEvents btnload As Button
+    Friend WithEvents btnclose As Button
+    Friend WithEvents datarecord As DataGridView
+End Class
---
+++ Employee1/Form1.vb
@@ -0,0 +1,38 @@
+﻿Imports MySql.Data.MySqlClient
+Imports Module1
+Public Class Form1
+
+    Dim sqlQuery As String
+    Dim da As New MySqlDataAdapter
+    Dim dt As DataTable
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DbConnect()
+
+    End Sub
+
+    Private Sub btnclose_Click(sender As Object, e As EventArgs) Handles btnclose.Click
+    End Sub
+
+    Private Sub btnload_Click(sender As Object, e As EventArgs) Handles btnload.Click
+        Try
+            DbConnect()
+            sqlQuery = " Select * from employee "
+            da = New MySqlDataAdapter(sqlQuery, Module1.conn)
+
+
+            dt = New DataTable
+            'fill the data from mysql table
+            da.Fill(dt)
+
+            'set the srouce of datagrid voew
+            datarecord.DataSource = dt
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+
+        End Try
+    End Sub
+End Class
