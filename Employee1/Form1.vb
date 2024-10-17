0ad5a2494878f83307ba1ae4704451ce728ca1a2
SANDBOX-09 <SANDBOX-09@SANDBOX-09>
2024-10-17 09:41:13 +0800

    10-17-24

--- Employee1/Form1.vb
+++ Employee1/Form1.vb
@@ -1,38 +1,61 @@
 ﻿Imports MySql.Data.MySqlClient
 Imports Module1
+
 Public Class Form1
 
     Dim sqlQuery As String
     Dim da As New MySqlDataAdapter
     Dim dt As DataTable
-
-
+    Dim conn As New MySqlConnection
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         DbConnect()
-
     End Sub
 
     Private Sub btnclose_Click(sender As Object, e As EventArgs) Handles btnclose.Click
+
+        Me.Close()
     End Sub
 
     Private Sub btnload_Click(sender As Object, e As EventArgs) Handles btnload.Click
         Try
-            DbConnect()
-            sqlQuery = " Select * from employee "
-            da = New MySqlDataAdapter(sqlQuery, Module1.conn)
-
+            sqlQuery = "SELECT * FROM employee"
+            da = New MySqlDataAdapter(sqlQuery, conn)
 
             dt = New DataTable
-            'fill the data from mysql table
             da.Fill(dt)
 
-            'set the srouce of datagrid voew
             datarecord.DataSource = dt
+            datarecord.AutoSizeColumnsMode = DataGridViewAutoSizeColumnMode.Fill
 
         Catch ex As Exception
             MsgBox(ex.Message)
-
         End Try
+    End Sub
+
+    Public Sub DbConnect()
+        Dim dbname As String = "it2a"
+        Dim username As String = "root"
+        Dim password As String = "admin"
+        Dim server As String = "127.0.0.1"
+
+        ' Establish new connection
+        conn.ConnectionString = "server=" & server & ";user id=" & username & ";password=" & password & ";database=" & dbname
+
+        Try
+            If conn.State = ConnectionState.Closed Then
+                conn.Open() ' Open connection
+                MsgBox("Connected")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnadd_Click(sender As Object, e As EventArgs) Handles btnadd.Click
+
+        Form2.Show()
+        Me.Hide() 'hide form 2
+
     End Sub
 End Class
---
+++ Employee1/Form2.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtfullname = New System.Windows.Forms.TextBox()
+        Me.txtusername = New System.Windows.Forms.TextBox()
+        Me.btnsave = New System.Windows.Forms.Button()
+        Me.btnback = New System.Windows.Forms.Button()
+        Me.txtpassword = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(80, 57)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(82, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Fullname"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(80, 112)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(91, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Username"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(80, 185)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(86, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Password"
+        '
+        'txtfullname
+        '
+        Me.txtfullname.Location = New System.Drawing.Point(214, 59)
+        Me.txtfullname.Name = "txtfullname"
+        Me.txtfullname.Size = New System.Drawing.Size(141, 20)
+        Me.txtfullname.TabIndex = 4
+        '
+        'txtusername
+        '
+        Me.txtusername.Location = New System.Drawing.Point(214, 114)
+        Me.txtusername.Name = "txtusername"
+        Me.txtusername.Size = New System.Drawing.Size(141, 20)
+        Me.txtusername.TabIndex = 5
+        '
+        'btnsave
+        '
+        Me.btnsave.Location = New System.Drawing.Point(313, 334)
+        Me.btnsave.Name = "btnsave"
+        Me.btnsave.Size = New System.Drawing.Size(92, 39)
+        Me.btnsave.TabIndex = 6
+        Me.btnsave.Text = "Save"
+        Me.btnsave.UseVisualStyleBackColor = True
+        '
+        'btnback
+        '
+        Me.btnback.Location = New System.Drawing.Point(431, 334)
+        Me.btnback.Name = "btnback"
+        Me.btnback.Size = New System.Drawing.Size(111, 39)
+        Me.btnback.TabIndex = 7
+        Me.btnback.Text = "Back"
+        Me.btnback.UseVisualStyleBackColor = True
+        '
+        'txtpassword
+        '
+        Me.txtpassword.Location = New System.Drawing.Point(214, 185)
+        Me.txtpassword.Name = "txtpassword"
+        Me.txtpassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtpassword.Size = New System.Drawing.Size(141, 20)
+        Me.txtpassword.TabIndex = 10
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.txtpassword)
+        Me.Controls.Add(Me.btnback)
+        Me.Controls.Add(Me.btnsave)
+        Me.Controls.Add(Me.txtusername)
+        Me.Controls.Add(Me.txtfullname)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form2"
+        Me.Text = "Add User"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtfullname As TextBox
+    Friend WithEvents txtusername As TextBox
+    Friend WithEvents btnsave As Button
+    Friend WithEvents btnback As Button
+    Friend WithEvents txtpassword As TextBox
+End Class
